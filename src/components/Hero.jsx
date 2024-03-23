3514a439fd9295610f48e6c89ae436ac653ffd1b
Nlucky320 <luckyes144@gmail.com>
2024-03-24 00:51:25 +0600

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="hero-content text-center">
         <div className="max-w-lg">
           <h1 className="text-5xl font-bold">
-            Welcome to <span>ByteBlaze</span>
+            Welcome to <span className=" bg-gradient-to-r from-primary via-blue-500 to-secondary text-transparent bg-clip-text bg-300% animate-gradient">ByteBlaze</span>
           </h1>
           <p className="py-6">
             ByteBlaze is the bridge between the complex world of technology and
